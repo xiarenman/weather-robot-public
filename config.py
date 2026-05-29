3e3efc3b11ce92b0bd4dba05f846721c3175692e
xiaren <1979157886@qq.com>
2026-05-29 10:41:00 +0800

    feat: 添加企业微信用户ID

--- config.py
+++ config.py
@@ -8,7 +8,7 @@ class Config:
     WECOM_CORP_ID = os.getenv("WECOM_CORP_ID", "wwf3c53a68a102c7ca")
     WECOM_AGENT_ID = os.getenv("WECOM_AGENT_ID", "1000002")
     WECOM_APP_SECRET = os.getenv("WECOM_APP_SECRET", "KVj_6AnLUd640d3TtZ0tyFoRmuySEKDpLI94tcHuWoA")
-    WECOM_TO_USER = os.getenv("WECOM_TO_USER", "YOUR_USER_ID_HERE")
+    WECOM_TO_USER = os.getenv("WECOM_TO_USER", "JiangChongHao")
 
     CITY = "上海"
     SCHEDULE_HOUR = 6
